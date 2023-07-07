d6bdbb620eb3722e8247561c5bd54dd2f190fea0
Markos Mk <markosmk@me.com>
2023-07-06 23:49:51 -0300

    add: getting data from goosheets

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,9 +1,39 @@
+import { checkEnvironment } from '@/config/data';
 import { QuoteForm } from './quote-form';
+import Papa from 'papaparse';
 
-export default function Page() {
+const parseData = (data: any) => {
+  return new Promise((resolve, reject) => {
+    Papa.parse(data, {
+      header: true,
+      complete: (results, file) => {
+        const response = results.data;
+        resolve(response);
+      },
+      error: (error, file) => {
+        reject(error.message);
+      },
+    });
+  });
+};
+
+async function getData() {
+  const res = await fetch(checkEnvironment().concat('/api'), {
+    next: { revalidate: 3600 }, // revalidate every hour
+  });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const data = await res.json();
+  const response = await parseData(data.response);
+  return response;
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <div className="grid grid-cols-1 gap-3">
-      <QuoteForm />
+      <QuoteForm content={data} />
     </div>
   );
 }
